3c341e99fd794431885b0502428ed74b60a367ca
olawale-o <omoogunolawale@gmail.com>
2022-03-27 13:56:02 +0100

    Create login form

---
+++ src/components/User/Login.js
@@ -0,0 +1,48 @@
+import { useFormik } from 'formik';
+import { model, initialValues, Schema } from '../../forms/Auth';
+
+const { login: { formField: { email, password } } } = model;
+
+const Login = () => {
+  const {
+    handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema: Schema,
+    onSubmit: (values) => {},
+  });
+  return(
+    <div className="login__container">
+      <form onSubmit={handleSubmit}>
+      <h1>Welcome back</h1>
+        <div className="switch-buttons">
+          <button type="button">Login</button>
+          <button type="button">Register</button>
+        </div>
+        <div className="continue-with">
+          <button type="button" className="google">Google</button>
+          <button type="button" className="facebook">Facebook</button>
+        </div>
+        <div className="or">
+          <span className="left" />
+          <span className="text">or</span>
+          <span className="right" />
+        </div>
+        <div className="field">
+          <CustomInput type="email" name={email.name} placeholder="Email" />
+        </div>
+        <div className="field">
+          <CustomInput type="password" name={password.name} placeholder="Password" />
+        </div>
+        <button
+          type="submit"
+          className="auth-btn"
+        >
+          Log in
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
