177ed9e14475c646d5652f1ebf63620a27906d28
olawale-o <omoogunolawale@gmail.com>
2022-03-27 16:20:58 +0100

    Render login and register forms

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Login, Register } from '../../components/User';
 
-const User = () => <div>User works</div>
+const User = () => {
+  const [isFocus, setIsFocus] = React.useState(false);
+  const onActive = () => {
+    setIsFocus(!isFocus);
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="container">
+        <div className={`form-container ${isFocus ? 'slide' : ''}`}>
+          <Login onActive={onActive} isFocus={isFocus} />
+          <Register onActive={onActive} isFocus={isFocus} />
+        </div>
+        <div className="overlay-container" />
+      </div>
+    </div>
+  );
+}
 
 export default User;
