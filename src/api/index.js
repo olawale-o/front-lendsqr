d39052929ebee384bfca701243696c69fb05982a
olawale-o <omoogunolawale@gmail.com>
2022-03-30 09:46:27 +0100

    Modify response

--- src/api/index.js
+++ src/api/index.js
@@ -6,12 +6,12 @@ const api = axios.create({ baseURL: BASE_URI });
 export const post = async (uri, options = {}) => {
   const { body = {} } = options;
   const response = await api.post(uri, body);
-  return response;
+  return response.data;
 };
 
 export const get = async (uri) => {
   const response = await api.get(uri);
-  return response;
+  return response.data;
 };
 
 export default api;
