c9b37418ef9f479527f7b5e5050a25780c692233
olawale-o <omoogunolawale@gmail.com>
2022-03-30 09:53:54 +0100

    Create storage

---
+++ src/storage/index.js
@@ -0,0 +1,5 @@
+export const setStorage = (key, value) => localStorage.setItem(key, value);
+
+export const getStorage = (key) => localStorage.getItem(key);
+
+export const removeStorage = (key) => localStorage.removeItem(key);
