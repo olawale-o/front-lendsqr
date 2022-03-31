3ee8067e691cbaccb0edcd9a1188241e21ab8068
olawale-o <omoogunolawale@gmail.com>
2022-03-31 11:54:36 +0100

    Modify account page

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import './style.css';
+import Sidebar from '../../components/SideBar';
 
-const Account = () => <div>Account works</div>;
-
+const Account = () => {
+  return (
+    <div className="account">
+      <Sidebar />
+      <div className="outlet"><Outlet /></div>
+    </div>
+  );
+}
 export default Account;
