c44b89bd8f172aad84d5c799f6ea04aa8f3112c4
olawale-o <omoogunolawale@gmail.com>
2022-04-01 10:31:31 +0100

    Pass currentUser props

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import './style.css';
 import Sidebar from '../../components/SideBar';
+import { currentUserSelector } from '../../store'
 
 const Account = () => {
+  const currentUser = useRecoilValue(currentUserSelector);
   return (
     <div className="account">
-      <Sidebar />
+      <Sidebar currentUser={currentUser} />
       <div className="outlet"><Outlet /></div>
     </div>
   );
