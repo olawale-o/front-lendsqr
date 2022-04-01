1f6d39e681af6bb428964397888211723f40fe4b
olawale-o <omoogunolawale@gmail.com>
2022-04-01 13:35:28 +0100

    Fix export error

--- src/forms/Account/index.js
+++ src/forms/Account/index.js
@@ -1,3 +1,3 @@
 export { default as model } from './model';
-export { default as schema } from './Schema';
+export { default as Schema } from './Schema';
 export { default as initialValues } from './initialValues';
