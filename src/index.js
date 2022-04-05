142c2240dcae512b60143e6092224f3ad0b29cc5
olawale-o <omoogunolawale@gmail.com>
2022-04-05 14:23:45 +0100

    Migrate project to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import './index.css';
@@ -27,15 +27,16 @@ api.interceptors.response.use((response) => response, (error) => {
   throw error;
 });
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <Router>
       <RecoilRoot>
         <App />
       </RecoilRoot>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
