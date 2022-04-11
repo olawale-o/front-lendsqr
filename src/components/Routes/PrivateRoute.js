e66794afef51c38f9337ec5e0677958fd89331ef
olawale-o <omoogunolawale@gmail.com>
2022-04-11 04:01:00 +0100

    Redirect on url params change

--- src/components/Routes/PrivateRoute.js
+++ src/components/Routes/PrivateRoute.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import PropType from 'prop-types';
 import currentUserSelector from '../../store/selectors/currentUserSelector';
 
 const PrivateRoute = ({ children }) => {
+  const location = useLocation();
   const user = useRecoilValue(currentUserSelector);
-  return user ? children : <Navigate to="/" />;
+  const current = location.pathname.split('')[1];
+  return user && user.id.toString() === current ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
